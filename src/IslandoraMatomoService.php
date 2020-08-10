275fa3a77211a4d337d1311bef1f42b23ece57c4
Bryan J. Brown <bjbrown@fsu.edu>
2020-08-10 20:10:35 +0000

    Services rough draft working

---
+++ src/IslandoraMatomoService.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Drupal\islandora_matomo_services;
+
+use Drupal\node\Entity\Node;
+
+/**
+ * Class IslandoraMatomoService.
+ */
+class IslandoraMatomoService implements IslandoraMatomoServiceInterface {
+
+  /**
+   * Constructs a new IslandoraMatomoService object.
+   */
+  public function __construct() {
+  }
+
+  public function queryMatomoApi($url, $mode) {
+    $url = rtrim($url, '/');
+    $matomo_config = \Drupal::config('matomo.settings');
+    $matomo_url = $matomo_config->get('url_http');
+    $matomo_id = $matomo_config->get('site_id');
+    if ($matomo_url == '' || $matomo_id == '') {
+      drupal_set_message(t('Error: Matomo not configured. Please make sure Matomo URL and site ID are set.'), 'error');
+      return NULL;
+    }
+    else {
+      $current_date = date('Y-m-d', time());
+      $date_range = "2000-01-01,{$current_date}";
+      switch ($mode):
+        case 'views':
+          $query = "index.php?module=API&method=Actions.getPageUrl&pageUrl={$url}&idSite={$matomo_id}&period=range&date={$date_range}&format=json";
+          break;
+        case 'downloads':
+          $query = "index.php?module=API&method=Actions.getDownload&downloadUrl={$url}&idSite={$matomo_id}&period=range&date={$date_range}&format=json";
+          break;
+        default:
+          drupal_set_message(t('Error: Invalid mode "{$mode}" provided to islandora_matomo_service.'), 'error');
+          return NULL;         
+      endswitch;
+      $request_url = $matomo_url . $query;
+      $response = json_decode(file_get_contents($request_url), TRUE); 
+      $result = (int) $response[0]['nb_hits'];    
+      return $result;
+    }
+  }
+
+  public function getViewsForNode($nid) {
+    $node = Node::load($nid);
+    $path = \Drupal\Core\Url::fromRoute('entity.node.canonical', ['node' => $node->id()])->toString();
+    global $base_url;
+    $node_url = $base_url . $path;
+    $views = \Drupal::service('islandora_matomo_services.default')->queryMatomoApi($node_url, 'views');
+    return $views;
+  }
+
+  public function getDownloadsForFile($fid) {
+    $file = file_load($fid);
+    $file_uri = $file->getFileUri();
+    $file_url = file_create_url($file_uri);
+    $downloads = \Drupal::service('islandora_matomo_services.default')->queryMatomoApi($file_url, 'downloads');
+    return $downloads;
+  }
+
+  public function getSummedDownloadsForFiles($fids) {
+    $sum = 0;
+    foreach ($fids as $fid) {
+      $file_downloads = \Drupal::service('islandora_matomo_services.default')->getDownloadsForFile($fid);
+      global $sum;
+      $sum = $sum + $file_downloads;
+    }
+    return $sum;
+  }
+
+}
---
+++ src/IslandoraMatomoServiceInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\islandora_matomo_services;
+
+/**
+ * Interface IslandoraMatomoServiceInterface.
+ */
+interface IslandoraMatomoServiceInterface {
+
+  public function queryMatomoApi($url, $mode);
+  public function getViewsForNode($nid);
+  public function getDownloadsForFile($fid);
+  public function getSummedDownloadsForFiles($fids);
+
+}
