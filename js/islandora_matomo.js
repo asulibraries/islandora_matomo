9c110c4ed0ea8f6a1726654b910039710980083e
Willow Gillingham <wgilling@asu.edu>
2021-07-12 15:45:19 +0000

    adjusted javascript to only fire once per media event per user session

--- js/islandora_matomo.js
+++ js/islandora_matomo.js
@@ -5,15 +5,21 @@
 (function ($) {
 Drupal.behaviors.islandora_matomo = {
   attach: function (context, settings) {
-    
-    var video = document.getElementsByTagName('video')[0];
-    if (video) {        
-      video.addEventListener('play', (event) => {
+    $('audio').each( function() {
+        console.log("hello world of audio");
+        $(this, context).once('islandora_matomo').on("play", function() {
+          console.log("only fires once per play");
+        });
+    });
+    $('video').each( function() {
+        console.log("hello world of video");
         console.log('video found on page');
-        console.log('The Boolean paused property is now false. Either the ' +
-        'play() method was called or the autoplay attribute was toggled.');
-      }, {once: true});
-    }
+        $(this, context).once('islandora_matomo').on("play", function() {
+          console.log("only fires once per play");
+          console.log('The Boolean paused property is now false. Either the ' +
+            'play() method was called or the autoplay attribute was toggled.');
+        });
+    });
   }
 };
 
