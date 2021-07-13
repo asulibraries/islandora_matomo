c479bcfa903250b79cf4557684458aaa34ce0f72
Willow Gillingham <wgilling@asu.edu>
2021-07-13 22:56:24 +0000

    adjust tracking event for audio or video play - so that the value saved is the item path like items/213 for #195

--- js/islandora_matomo.js
+++ js/islandora_matomo.js
@@ -5,21 +5,30 @@
 (function ($) {
 Drupal.behaviors.islandora_matomo = {
   attach: function (context, settings) {
-    $('audio').each( function() {
-        console.log("hello world of audio");
-        $(this, context).once('islandora_matomo').on("play", function() {
-          console.log("only fires once per play");
-        });
-    });
-    $('video').each( function() {
-        console.log("hello world of video");
-        console.log('video found on page');
-        $(this, context).once('islandora_matomo').on("play", function() {
-          console.log("only fires once per play");
-          console.log('The Boolean paused property is now false. Either the ' +
-            'play() method was called or the autoplay attribute was toggled.');
-        });
-    });
+    // Since any of these would require the _paq library is defined -- and that
+    // depends on whether or not the user is configured to track Matomo events
+    // i.e. "do not add tracking for Admin role", this logic can wrap any
+    // further logic.
+    if (typeof _paq !== 'undefined') {
+      var pathname = window.location.pathname;
+      console.log("path = " + pathname);
+      pathname = pathname.substring(1, pathname.length);
+      console.log("path = " + pathname);
+      $('audio').each( function() {
+          $(this, context).once('islandora_matomo').on("play", function() {
+            // Track the event with Matomo.
+            _paq.push(['trackEvent', 'MediaEvents', 'Play audio', pathname]);
+            console.log("only fires once per play");
+          });
+      });
+      $('video').each( function() {
+          $(this, context).once('islandora_matomo').on("play", function() {
+            // Track the event with Matomo.
+            _paq.push(['trackEvent', 'MediaEvents', 'Play video', pathname]);
+            console.log("only fires once per play");
+          });
+      });
+    }
   }
 };
 
